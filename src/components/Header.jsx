813768b7e951a293d10f9ec61f33c60221aed790
aris <webuilder997@gmail.com>
2023-02-06 17:03:19 +0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import logo from "../assets/images/astronout.png"
+import bg from "../assets/images/bg.jpg"
+// import Navbar from './Navbar'
+import "../style.css"
+
+function Header() {
+  return (
+   <>
+   <div style={{backgroundImage: `url(${bg})`}} className=' bg-center bg-cover bg-no-repeat h-screen flex flex-col   '>
+    {/* <Navbar/> */}
+    <div className="wrapper flex justify-between items-center h-screen w-full px-20 lg:justify-center lg:px-6">
+      <div className="content lg:text-center">
+        <div className="btn py-3">
+          <button  className=' border-2 border-[#b0aaaa] font-bold text-white bg-[linear-gradient(90deg,#b004b0,#38097a)] p-4 rounded-2xl'>Welcome to my Portfolio</button>
+        </div>
+        <h1 className='text-white text-5xl font-bold'>Hi! Saya Aries</h1>
+        <p className='text-white py-4 max-w-lg '>Lorem ipsum dolor sit amet consectetur  adipisicing elit. In qui impedit  sunt, est molestiae ullam. Saepe voluptatum ducimus adipisci pariatur. adipisicing elit.  In qui impedit ipsum dolor sit amet consectetur</p>
+        </div>
+      <div className="image banner-astronout lg:hidden absolute w-[400px] right-28">
+          <img className='w-96' src={logo} alt="" />
+      </div>
+    </div>
+   </div>
+   </>
+  )
+}
+
+export default Header
